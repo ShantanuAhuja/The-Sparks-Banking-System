c875a2ae0e2fe491e7f16acc4909eb33f107b12b
Shantanu Ahuja <ahujashantanu2904@gmail.com>
2022-02-07 10:36:17 +0530

    Update sparks_bank.sql

--- sparks_bank.sql
+++ sparks_bank.sql
@@ -1,24 +1,14 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: May 19, 2021 at 10:01 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.33
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+
 
 --
--- Database: `sparks_bank`
+-- Database: `bank user`
 --
 
 -- --------------------------------------------------------
@@ -97,6 +87,4 @@ ALTER TABLE `users`
   MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
